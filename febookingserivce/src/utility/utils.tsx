8d8920b93707fddd4a24bd0ef4f77db40bc9f0ec
idslayer <29697148@students.lincoln.ac.uk>
2025-08-25 22:24:28 +0000

    test deploy

--- febookingserivce/src/utility/utils.tsx
+++
@@ -1,8 +0,0 @@
-
-export const buildPageParams = (page: number, size: number, sorts: string[]) => {
-  const p = new URLSearchParams();
-  p.append('page', String(page));
-  p.append('size', String(size));
-  sorts.forEach(s => p.append('sort', s)); // sort=field,dir (repeat)
-  return p;
-};
